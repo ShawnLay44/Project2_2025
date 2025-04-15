52fcaeee91e012abf58e76615cdfa06a547e9721
Wulei <kobe13678010522@163.com>
2025-04-15 19:53:08 +0800

    Add SoilSensor.py to repository

--- SoilSensor.py
+++ SoilSensor.py
@@ -1,18 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-channel = 21
+channel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
 	if GPIO.input(channel):
-		print ("Water Detected!")
+		print ("No Water Detected!")
 	else:
 		print ("Water Detected!")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(channel, callback)
 
-while true:
-	time,sleep(1)
+while True:
+	time.sleep(0)
